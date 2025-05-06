55446988e4e98c6e7c248ee870453040f5c3078b
yusay1498 <u.yusay1498@gmail.com>
2025-05-06 10:29:16 +0900

    fix counter item style

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,9 +4,11 @@ function Counter({ counter, onIncrement, onDecrement }) {
     return (
         <div className="counter-container">
             <h2 className="counter-name">{counter.name}</h2>
-            <button className="counter-button decrement" onClick={onDecrement}>-</button>
-            <span className="counter-value">{counter.value}</span>
-            <button className="counter-button increment" onClick={onIncrement}>+</button>
+            <div className="counter-controls">
+                <button className="counter-button decrement" onClick={onDecrement}>-</button>
+                <span className="counter-value">{counter.value}</span>
+                <button className="counter-button increment" onClick={onIncrement}>+</button>
+            </div>
         </div>
     );
 }
